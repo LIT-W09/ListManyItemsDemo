4fce7b66936f4d8c09e323508559cf1ffed17ca9
Avrumi Friedman <a.friedman07@gmail.com>
2022-03-09 21:34:17 -0500

    Initial commit

---
+++ ListManyFoodItems.Data/FoodItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ListManyFoodItems.Data
+{
+    public class FoodItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CalorieCount { get; set; }
+    }
+
+    public class FoodItemDb
+    {
+        private string _connectionString;
+        public FoodItemDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddFoodItem(FoodItem item)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO FoodItems (Name, Price, CalorieCount) " +
+                "VALUES (@name, @price, @calories)";
+
+            cmd.Parameters.AddWithValue("@name", item.Name);
+            cmd.Parameters.AddWithValue("@price", item.Price);
+            cmd.Parameters.AddWithValue("@calories", item.CalorieCount);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
---
+++ ListManyFoodItems.Web/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using ListManyFoodItems.Data;
+using ListManyFoodItems.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListManyFoodItems.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString = "Data Source=.\\sqlexpress;Initial Catalog=SnackStore;Integrated Security=True";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFood(FoodItem item)
+        {
+            var db = new FoodItemDb(_connectionString);
+            db.AddFoodItem(item);
+            return Redirect("/");
+        }
+
+        public IActionResult ShowAddManyNumbers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostInts(List<int> numbers)
+        {
+            return Redirect("/home/showaddmanynumbers");
+        }
+
+        public IActionResult ShowAddManyFoodItems()
+        {
+            var vm = new ShowAddManyViewModel();
+            string message = (string)TempData["Message"];
+            if (!String.IsNullOrEmpty(message))
+            {
+                vm.Message = message;
+            }
+            
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult AddMultipleFoodItems(List<FoodItem> foodItems)
+        {
+            var db = new FoodItemDb(_connectionString);
+            foreach (var item in foodItems)
+            {
+                db.AddFoodItem(item);
+            }
+            TempData["Message"] = $"{foodItems.Count} items added successfully!";
+            return Redirect("/home/ShowAddManyFoodItems");
+        }
+    }
+
+}
